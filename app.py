d9bbc215b74d8e402e64dd499c93ac830a9c9ac2
Matheus <matheusbnvr@gmail.com>
2024-02-19 23:10:16 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+
+def exibir_nome_do_programa():
+    print('''
+      
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    ''')
+
+def exibir_opcoes():
+    print('1- Cadastrar restaurante')
+    print('2- Listar restaurante')
+    print('3- Ativar restaurante')
+    print('4- Sair\n')
+
+#Função criada para limpar o terminal
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o App...\n')
+
+def escolher_opcoes():
+    opcao_escolhida = int(input ('Escolha uma opção: '))
+
+    #Condicional para seleção de opções
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurantes')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+#-----------------------------------#
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
